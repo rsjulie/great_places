ef4aac32a3bd109e72b1d56c2d6c191b3c3356d0
Julie <julie.ribeiro.silva@gmail.com>
2022-03-31 12:04:30 -0300

    Database Connection

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -3,9 +3,28 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:great_places/models/place.dart';
+import 'package:great_places/utils/constants.dart';
+import 'package:great_places/utils/db_util.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
+
+  Future<void> loadPlaces() async {
+    final dataList = await DbUtil.getData(Constants.TABLE_NAME);
+    _items = dataList
+        .map((item) => Place(
+              id: item['id'],
+              title: item['title'],
+              image: File(item['image']),
+              location: PlaceLocation(
+                latitud: 0.0,
+                longitud: 0.0,
+              ),
+            ))
+        .toList();
+    notifyListeners();
+  }
+
   List<Place> get items {
     return [..._items];
   }
@@ -23,10 +42,19 @@ class GreatPlaces with ChangeNotifier {
       id: Random().nextDouble().toString(),
       title: title,
       image: image,
-      location: null,
+      location: PlaceLocation(
+        latitud: 0.0,
+        longitud: 0.0,
+      ),
     );
 
     _items.add(newPlace);
+    DbUtil.insert(Constants.TABLE_NAME, {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
+
     notifyListeners();
   }
 }
